d8177ab0e6c19a655b369bda26a4dab42c7a0d1d
Roja <2021mt93264@wilp.bits-pilani.ac.in>
2022-04-17 21:41:58 +0530

    add webelement code

--- Selenium_Assignment.py
+++ Selenium_Assignment.py
@@ -7,41 +7,36 @@ class TestAutomation:
 
     def launch_browser_verify_functionality(self):
         try:
-            # driver = webdriver.Chrome(executable_path='E:\\Automation directory\\chromedriver.exe')
-           # driver = webdriver.edge(executable_path="D:\\msedgedriver.exe")
             driver = webdriver.Chrome(executable_path='D:\\chromedriver.exe')
             driver.get("https://rahulshettyacademy.com/")
             driver.maximize_window()
-            time.sleep(15)
-            if driver.current_url != "https://rahulshettyacademy.com/":
+            time.sleep(5)
+            if driver.current_url!="https://rahulshettyacademy.com/":
                 raise ("Page not laod successfully")
             else:
                 driver.find_element(by=By.XPATH,
-                                    value="//div[@class='login-btn']/a[@class='theme-btn register-btn']").click()  # Identified webelement using Xpath
+                                       value="//div[@class='login-btn']/a[@class='theme-btn register-btn']").click()  # Identified webelement using Xpath
                 time.sleep(5)
                 driver.find_element(by=By.NAME, value="email").send_keys("2021mt93264@wilp.bits-pilani.ac.in")
                 driver.find_element(by=By.NAME, value="password").send_keys("oneIlmg@12")
                 driver.find_element(by=By.NAME, value="commit").click()
-                time.sleep(3)
+                time.sleep(20)
                 driver.find_element(by=By.XPATH,
                                     value="(//div[@class='navbar-collapse collapse clearfix']/ul/li/a[contains(text(),'Practice')])[1]").click()
                 driver.find_element(by=By.NAME, value="name").send_keys("Roja")
                 driver.find_element(by=By.NAME, value="email").send_keys("2021mt93264@wilp.bits-pilani.ac.in")
-                time.sleep(20)
+                time.sleep(15)
                 driver.find_element(by=By.XPATH,
-                                    value="//button[@id='form-submit']").click()  # Identified webelement using Xpath
+                                    value="//button[@id ='form-submit']").click()  # Identified webelement using Xpath
                 time.sleep(5)
                 driver.find_element(by=By.XPATH,
                                     value="//div[@class='projects-item']/a[contains(text(),'Automation Practise - 2')]").click()
                 time.sleep(5)
-                list_count = driver.find_elements(by=By.XPATH,
-                                                  value="//legend[contains(text(),'Web Table Fixed header')]/../div/table/thead/tr")
+                list_count = driver.find_elements(by=By.XPATH, value="//legend[contains(text(),'Web Table Fixed header')]/../div/table/thead/tr")
                 print(len(list_count))
-                print(list_count)
 
         except Exception as e:
             print("Page not load successfully")
-
 
 object_load = TestAutomation()
 object_load.launch_browser_verify_functionality()
